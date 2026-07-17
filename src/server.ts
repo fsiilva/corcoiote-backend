a052750cc99f4b86bbebc636c7e685a5b081588e
fsiilva <f.siiilva@outlook.com>
2026-07-17 19:14:33 -0300

    ar condicionado fazendo zuada

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,28 @@
-import http from 'node:http';
+import express from "express";
+const app = express();
+app.use(express.json());
+
+app.get('/customers', (resquest, response) => {
+    const customers = [
+        {
+            nome: "Antonio",
+            status: false
+        },
+        {
+            nome: "Flavio",
+            status: true
+        },
+        {
+            nome: "Silva",
+            status: false
+        },
+        {
+            nome: "Patricio",
+            status: true
+        }
+    ];
+
+    response.status(200).json(customers);
+});
+
+app.listen(3000);
